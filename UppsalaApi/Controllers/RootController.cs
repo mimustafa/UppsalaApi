ac90ad0d053508dca77ecae9ba43976298780587
mimustafa <mudassirimran@gmail.com>
2018-10-26 20:41:21 +0200

    Initial check-in of module UppsalaApi

---
+++ UppsalaApi/Controllers/RootController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace UppsalaApi.Controllers
+{
+    [Route("/")]
+    public class RootController : Controller
+    {
+        [HttpGet(Name =  nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var resposne = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(resposne);
+
+        }
+ 
+    }
+}
